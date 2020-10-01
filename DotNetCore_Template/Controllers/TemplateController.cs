73f9c51ab310f5c793e8572aef909c2b87af7f48
Grieco <grieco@intre.it>
2020-10-01 16:59:32 +0200

    Setup template

---
+++ DotNetCore_Template/Controllers/TemplateController.cs
@@ -0,0 +1,21 @@
+﻿using MiaServiceDotNetLibrary;
+using MiaServiceDotNetLibrary.Decorators;
+using MiaServiceDotNetLibrary.Environment;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore_Template.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemplateController : ServiceStatusController
+    {
+        public TemplateController(MiaEnvConfiguration miaEnvConfiguration, ServiceClientFactory serviceClientFactory,
+            DecoratorResponseFactory decoratorResponseFactory) : base(miaEnvConfiguration, serviceClientFactory,
+            decoratorResponseFactory)
+        {
+            /*
+            * Insert your code here.
+            */
+        }
+    }
+}
